91d8e1a163e200e88c830d3efb82b4e378d9a6d3
Austen <26863753+Austenbe@users.noreply.github.com>
2026-05-23 23:28:13 -0500

    Cloud Build Errors

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,5 @@
 import { MongoClient } from "mongodb"
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your MongoDB URI to .env.local")
-}
 
 const uri = process.env.MONGODB_URI
 const options = {}
